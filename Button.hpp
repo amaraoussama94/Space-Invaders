82fa09ee8a475ae9926f606ab4183e7d13320272
amaraoussama94 <wassa36@yahoo.fr>
2022-11-16 15:00:01 +0100

    adding the  buttons function

--- Button.hpp
+++ Button.hpp
@@ -9,7 +9,7 @@ private:
     Font m_Font;
 public:
     std::string m_Text;
-    FloatRect m_Collider;
+    FloatRect m_Collider;//used to detect mouse clicks on the button
     Button(Vector2f position,float width, float height,int red, int green, int blue,std::string text);
     void draw(RenderWindow& window);
 };
