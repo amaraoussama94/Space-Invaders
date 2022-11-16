72ff51e0916ef25b78766996507ee8ac9ffcbc5c
amaraoussama94 <wassa36@yahoo.fr>
2022-11-16 17:12:57 +0100

    select uipanel class creation

---
+++ SelectUIPanel.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include "UIPanel.hpp"
+class SelectUIPanel : public UIPanel
+{
+private:
+    void initialiseButtons();
+public:
+    SelectUIPanel(Vector2i res);
+    void virtual draw(RenderWindow& window);
+};
