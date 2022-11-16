d2fe9971ff7c2660c73141b34e7606ac4f064c23
amaraoussama94 <wassa36@yahoo.fr>
2022-11-16 17:06:52 +0100

    adding  select input handler  function

--- SelectInputHandler.cpp
+++ SelectInputHandler.cpp
@@ -1,1 +1,30 @@
- 
+ #include "SelectInputHandler.hpp"
+#include "SoundEngine.hpp"
+#include "WorldState.hpp"
+#include <iostream>
+
+int WorldState::WAVE_NUMBER;
+
+void SelectInputHandler::handleKeyPressed(Event& event, RenderWindow& window)
+{
+    // Quit the game
+    if (Keyboard::isKeyPressed(Keyboard::Escape))
+    {
+        window.close();
+    }
+}
+
+void SelectInputHandler::handleLeftClick(std::string& buttonInteractedWith, RenderWindow& window)
+{
+    if (buttonInteractedWith == "Play") 
+    {
+        SoundEngine::playClick();
+        WorldState::WAVE_NUMBER = 0;
+        getPointerToScreenManagerRemoteControl()->loadLevelInPlayMode("level1");
+    }
+    if (buttonInteractedWith == "Quit") 
+    {
+        SoundEngine::playClick();
+        window.close();
+    }
+}
