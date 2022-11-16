a722884d4ebcfd835396f68eb588dc8f22ca19d1
amaraoussama94 <wassa36@yahoo.fr>
2022-11-16 16:44:01 +0100

    adding screen function

--- Screen.hpp
+++ Screen.hpp
@@ -8,7 +8,7 @@
 class InputHandler;class Screen
 {
 private:
-    vector<shared_ptr<InputHandler>> m_InputHandlers;
+    vector<shared_ptr<InputHandler>> m_InputHandlers;//This is where we will store all the derived InputHandler instances
     vector<unique_ptr<UIPanel>> m_Panels;
 protected:
     void addPanel(unique_ptr<UIPanel> p,ScreenManagerRemoteControl* smrc,shared_ptr<InputHandler> ih);
