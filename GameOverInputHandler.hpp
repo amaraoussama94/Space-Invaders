c26ca25f431279242f6bfc1072311c1632b9cfa9
amaraoussama94 <wassa36@yahoo.fr>
2022-11-17 14:38:57 +0100

    fix bugs

--- GameOverInputHandler.hpp
+++ GameOverInputHandler.hpp
@@ -1,11 +1,13 @@
 #pragma once
 #include "InputHandler.hpp"
-#include "SoundEngine.hpp"
-#include "WorldState.hpp"
-#include <iostream>
-class GameOverInputHandler :public InputHandler
+
+class GameOverInputHandler :
+	public InputHandler
 {
 public:
-    void handleKeyPressed(Event& event,RenderWindow& window) override;
-    void handleLeftClick(std::string&buttonInteractedWith, RenderWindow& window) override;
+	void handleKeyPressed(Event& event,
+		RenderWindow& window) override;
+
+	void handleLeftClick(std::string&
+		buttonInteractedWith, RenderWindow& window) override;
 };
--- GameOverUIPanel.hpp
+++ GameOverUIPanel.hpp
@@ -1,10 +1,12 @@
 #pragma once
 #include "UIPanel.hpp"
-class GameOverUIPanel :public UIPanel
+class GameOverUIPanel :
+	public UIPanel
 {
 private:
-    void initialiseButtons();
+	void initialiseButtons();
+
 public:
-    GameOverUIPanel(Vector2i res);
-    void virtual draw(RenderWindow& window);
+	GameOverUIPanel(Vector2i res);
+	void virtual draw(RenderWindow& window);
 };
