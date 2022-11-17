6a90f115946ee2eb84abf27720b69ce16fc974c5
amaraoussama94 <wassa36@yahoo.fr>
2022-11-17 08:26:21 +0100

    adding the game screen class

---
+++ GameScreen.hpp
@@ -0,0 +1,19 @@
+#pragma once
+#include "Screen.hpp"
+#include "GameInputHandler.hpp"
+#include "GameOverInputHandler.hpp"
+
+class GameScreen : public Screen
+{
+private:
+    ScreenManagerRemoteControl* m_ScreenManagerRemoteControl;
+    shared_ptr<GameInputHandler> m_GIH;
+    Texture m_BackgroundTexture;
+    Sprite m_BackgroundSprite;
+public:
+    static bool m_GameOver;
+    GameScreen(ScreenManagerRemoteControl* smrc, Vector2i res);
+    void initialise() override;
+    void virtual update(float fps);
+    void virtual draw(RenderWindow& window);
+};
