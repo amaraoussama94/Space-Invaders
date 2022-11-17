3bf3275265ddba214c098ec5f65caf4990b526c0
amaraoussama94 <wassa36@yahoo.fr>
2022-11-17 09:14:47 +0100

    adding the  game input  handler function

--- GameInputHandler.cpp
+++ GameInputHandler.cpp
@@ -1,1 +1,26 @@
- 
+#include "GameInputHandler.hpp"
+#include "SoundEngine.hpp"
+#include "GameScreen.hpp"
+
+void GameInputHandler::initialize() 
+{
+
+}
+void GameInputHandler::handleGamepad()
+{
+}
+void GameInputHandler::handleKeyPressed(Event& event, RenderWindow& window)
+{
+    // Handle key presses
+    if (event.key.code == Keyboard::Escape)
+    {
+        //When the player presses the Escape key, 
+        //the ScreenMangerRemoteControl pointer calls the
+        //SwitchScreen function to go back to the select screen
+        SoundEngine::playClick();
+        getPointerToScreenManagerRemoteControl()->SwitchScreens("Select");
+    }
+}
+void GameInputHandler::handleKeyReleased(Event& event, RenderWindow& window)
+{
+}
