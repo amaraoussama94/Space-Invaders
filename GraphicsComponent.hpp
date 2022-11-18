54306d1a72357298ce104a3cff290f8debc720d1
amaraoussama94 <wassa36@yahoo.fr>
2022-11-18 11:47:16 +0100

    ading the graphics Component class

---
+++ GraphicsComponent.hpp
@@ -0,0 +1,47 @@
+#pragma once
+#include "Component.hpp"
+#include "TransformComponent.hpp"
+#include "GameObjectSharer.hpp"
+
+#include <SFML/Graphics.hpp>
+
+#include <string>
+#include <iostream>
+#include <memory>
+
+using namespace sf;
+using namespace std;
+
+
+class GraphicsComponent : public Component {
+private:
+    string m_Type = "graphics";
+    bool m_Enabled = false;
+public:
+    virtual void draw(RenderWindow& window,shared_ptr<TransformComponent> t) = 0;
+    virtual void initializeGraphics(string bitmapName,Vector2f objectSize) = 0;
+
+    /****************************************************
+    *****************************************************
+    From Component interface
+    *****************************************************
+    *****************************************************/
+    string  getType() 
+    {
+        return m_Type;
+    }
+    void  disableComponent() 
+    {
+        m_Enabled = false;
+    }
+    void  enableComponent() 
+    {
+        m_Enabled = true;
+    }
+    bool  enabled() 
+    {
+        return m_Enabled;
+    }
+    void  start(GameObjectSharer* gos, GameObject* self) 
+    {}
+};
