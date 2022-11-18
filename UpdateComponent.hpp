1280dffd13a7960338f2e0d8dc614bb943f47002
amaraoussama94 <wassa36@yahoo.fr>
2022-11-18 14:16:00 +0100

    adding the  update  component class

---
+++ UpdateComponent.hpp
@@ -0,0 +1,35 @@
+ #pragma once
+#include "Component.hpp"
+
+class UpdateComponent : public Component
+{
+private:
+    string m_Type = "update";
+    bool m_Enabled = false;
+public:
+    virtual void update(float fps) = 0;
+    /****************************************************
+    *****************************************************
+    From Component interface
+    *****************************************************
+    *****************************************************/
+    string getType() 
+    {
+        return m_Type;
+    }
+    void  disableComponent() 
+    {
+        m_Enabled = false;
+    }
+    void  enableComponent() 
+    {
+        m_Enabled = true;
+    }
+    bool  enabled() 
+    {
+        return m_Enabled;
+    }
+    void  start(GameObjectSharer* gos, GameObject* self) 
+    {
+    }
+};
