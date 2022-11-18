82b8d060efc85b6facb5fcbeedef1751176f7c6b
amaraoussama94 <wassa36@yahoo.fr>
2022-11-18 14:00:00 +0100

    adding the transform component

---
+++ TransformComponent.hpp
@@ -0,0 +1,44 @@
+#pragma once
+#include "Component.hpp"
+#include<SFML/Graphics.hpp>
+using namespace sf;
+
+class Component;
+
+class TransformComponent : public Component 
+{
+private:
+    const string m_Type = "transform";
+    Vector2f m_Location;
+    float m_Height;
+    float m_Width;
+public:
+    TransformComponent(float width, float height, Vector2f location);
+    Vector2f& getLocation();
+    Vector2f getSize();
+    /****************************************************
+    *****************************************************
+    From Component interface
+    *****************************************************
+    *****************************************************/
+    string getType()
+    {
+        return m_Type;
+    }
+
+    string getSpecificType()
+    {
+        // Only one type of Transform so just return m_Type
+        return m_Type;
+    }
+    void disableComponent()
+    {}
+    void enableComponent()
+    {}
+    bool enabled()
+    {
+        return false;
+    }
+    void start(GameObjectSharer* gos, GameObject* self)
+    {}
+};
