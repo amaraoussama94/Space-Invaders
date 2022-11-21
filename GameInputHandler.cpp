f4d9f7b08a239ca64883d086ff8a35fce1cd509f
amaraoussama94 <wassa36@yahoo.fr>
2022-11-21 12:06:25 +0100

    adding the  controle of the game in the input handler

--- GameInputHandler.cpp
+++ GameInputHandler.cpp
@@ -2,6 +2,8 @@
 #include "SoundEngine.hpp"
 #include "GameScreen.hpp"
 
+class BulletSpawner;
+
 void GameInputHandler::initialize() 
 {
 
@@ -19,6 +21,22 @@ void GameInputHandler::handleKeyPressed(Event& event, RenderWindow& window)
         //SwitchScreen function to go back to the select screen
         SoundEngine::playClick();
 		getPointerToScreenManagerRemoteControl()->SwitchScreens("Select");
+    }
+    if (event.key.code == Keyboard::Left)
+    {
+        m_PUC->moveLeft();
+    }
+    if (event.key.code == Keyboard::Right)
+    {
+        m_PUC->moveRight();
+    }
+    if (event.key.code == Keyboard::Up)
+    {
+        m_PUC->moveUp();
+    }
+    if (event.key.code == Keyboard::Down)
+    {
+        m_PUC->moveDown();
     }
 }
 void GameInputHandler::handleKeyReleased(Event& event, RenderWindow& window)
