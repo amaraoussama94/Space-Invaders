4cdafb4d7e197a17fceceaba2157bd13e3b37513
amaraoussama94 <wassa36@yahoo.fr>
2022-11-21 12:03:07 +0100

    update the game inpute handler class with 2 new component

--- GameInputHandler.hpp
+++ GameInputHandler.hpp
@@ -1,9 +1,15 @@
 #pragma once
 #include "InputHandler.hpp"
+#include "PlayerUpdateComponent.hpp"
+#include "TransformComponent.hpp"
 class GameScreen;
 
 class GameInputHandler : public InputHandler
 {
+private:
+    shared_ptr<PlayerUpdateComponent> m_PUC;
+    shared_ptr<TransformComponent> m_PTC;
+    bool mBButtonPressed = false;
 public:
     void initialize();
     void handleGamepad() override;
