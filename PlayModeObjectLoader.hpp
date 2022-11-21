441098135b4dfff582ada1d296ce661ba668b01b
amaraoussama94 <wassa36@yahoo.fr>
2022-11-21 09:45:19 +0100

    adding the play mode object loader class

---
+++ PlayModeObjectLoader.hpp
@@ -0,0 +1,15 @@
+#pragma once
+#include <vector>
+#include <string>
+#include "GameObject.hpp"
+#include "BlueprintObjectParser.hpp"
+#include "GameObjectFactoryPlayMode.hpp"
+using namespace std;
+
+class PlayModeObjectLoader {
+private:
+    BlueprintObjectParser m_BOP;
+    GameObjectFactoryPlayMode m_GameObjectFactoryPlayMode;
+public:
+    void loadGameObjectsForPlayMode(string pathToFile, vector<GameObject>& mGameObjects);
+};
