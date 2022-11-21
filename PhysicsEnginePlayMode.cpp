5715063d08799414466a4e1b7d7001ea3fe155be
amaraoussama94 <wassa36@yahoo.fr>
2022-11-21 14:44:33 +0100

    adding all function of physics engine play mode

--- PhysicsEnginePlayMode.cpp
+++ PhysicsEnginePlayMode.cpp
@@ -1,20 +1,42 @@
-#pragma once
-#include "GameObjectSharer.hpp"
-#include "PlayerUpdateComponent.hpp"
+#include <iostream>
 
-class PhysicsEnginePlayMode
+#include "DevelopState.hpp"
+#include "PhysicsEnginePlayMode.hpp"
+#include "SoundEngine.hpp"
+#include "WorldState.hpp"
+#include "InvaderUpdateComponent.hpp"
+#include "BulletUpdateComponent.hpp"
+
+void PhysicsEnginePlayMode::detectInvaderCollisions(vector<GameObject>& objects,const vector<int>& bulletPositions)
 {
-private:
-    shared_ptr<PlayerUpdateComponent> m_PUC;
-    GameObject* m_Player;
-    bool m_InvaderHitWallThisFrame = false;
-    bool m_InvaderHitWallPreviousFrame = false;
-    bool m_NeedToDropDownAndReverse = false;
-    bool m_CompletedDropDownAndReverse = false;
-    void detectInvaderCollisions(vector<GameObject>& objects,const vector<int>& bulletPositions);
-    void detectPlayerCollisionsAndInvaderDirection(vector<GameObject>& objects,const vector<int>& bulletPositions);
-    void handleInvaderDirection();
-public:
-    void initilize(GameObjectSharer& gos);
-    void detectCollisions(vector<GameObject>& objects,const vector<int>& bulletPositions);
-};
+    Vector2f offScreen(-1, -1);
+    auto invaderIt = objects.begin();
+    auto invaderEnd = objects.end();
+    for (invaderIt;invaderIt != invaderEnd;++invaderIt)
+    {
+        if ((*invaderIt).isActive()&& (*invaderIt).getTag() == "invader")
+        {
+            auto bulletIt = objects.begin();
+            // Jump to the first bullet
+            advance(bulletIt, bulletPositions[0]);
+            auto bulletEnd = objects.end();
+            for (bulletIt;bulletIt != bulletEnd;++bulletIt)
+            {
+                if ((*invaderIt).getEncompassingRectCollider().intersects((*bulletIt).getEncompassingRectCollider())
+                && (*bulletIt).getTag() == "bullet"
+                && static_pointer_cast<
+                BulletUpdateComponent>(
+                (*bulletIt).getFirstUpdateComponent())
+                ->m_BelongsToPlayer)
+                {
+                    SoundEngine::playInvaderExplode();
+                    (*invaderIt).getTransformComponent()->getLocation() = offScreen;
+                    (*bulletIt).getTransformComponent()->getLocation() = offScreen;
+                    WorldState::SCORE++;
+                    WorldState::NUM_INVADERS--;
+                    (*invaderIt).setInactive();
+                }
+            }
+        }
+    }
+}
