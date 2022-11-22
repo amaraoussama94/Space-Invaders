d37f9081ce05cd64a2b1f881450ed6f7358c25e0
amaraoussama94 <wassa36@yahoo.fr>
2022-11-22 17:16:51 +0100

    adding some missing var and fix typo

--- GameObjectBlueprint.cpp
+++ GameObjectBlueprint.cpp
@@ -1,88 +1,77 @@
- 
 #include "GameObjectBlueprint.hpp"
 
-float GameObjectBlueprint::getWidth()
-{
-return m_Width;
+float GameObjectBlueprint::getWidth() {
+	return m_Width;
 }
 
-void GameObjectBlueprint::setWidth(float width)
-{
-m_Width = width;
+void GameObjectBlueprint::setWidth(float width) {
+	m_Width = width;
 }
 
-float GameObjectBlueprint::getHeight()
-{
-return m_Height;
+float GameObjectBlueprint::getHeight() {
+	return m_Height;
 }
 
-void GameObjectBlueprint::setHeight(float height)
-{
-m_Height = height;
+void GameObjectBlueprint::setHeight(float height) {
+	m_Height = height;
 }
 
-float GameObjectBlueprint::getLocationX()
-{
-return m_LocationX;
+float GameObjectBlueprint::getLocationX() {
+	return m_LocationX;
 }
 
-void GameObjectBlueprint::setLocationX(float locationX)
-{
-m_LocationX = locationX;
+void GameObjectBlueprint::setLocationX(float locationX) {
+	m_LocationX = locationX;
 }
 
-float GameObjectBlueprint::getLocationY()
-{
-return m_LocationY;
+float GameObjectBlueprint::getLocationY() {
+	return m_LocationY;
 }
 
-void GameObjectBlueprint::setLocationY(float locationY)
-{
-m_LocationY = locationY;
+void GameObjectBlueprint::setLocationY(float locationY) {
+	m_LocationY = locationY;
 }
+
 
 void GameObjectBlueprint::setName(string name)
 {
-m_Name = "" + name;
+	m_Name = "" + name;
 }
 
 string GameObjectBlueprint::getName()
 {
-return m_Name;
+	return m_Name;
 }
 
 vector<string>& GameObjectBlueprint::getComponentList()
 {
-return m_ComponentList;
+	return m_ComponentList;
 }
 
 void GameObjectBlueprint::addToComponentList(string newComponent)
 {
-m_ComponentList.push_back(newComponent);
+	m_ComponentList.push_back(newComponent);
 }
 
 string GameObjectBlueprint::getBitmapName()
 {
-return m_BitmapName;
+	return m_BitmapName;
 }
 
 void GameObjectBlueprint::setBitmapName(string bitmapName)
 {
-m_BitmapName = "" + bitmapName;
+	m_BitmapName = "" + bitmapName;
 }
 
-string GameObjectBlueprint::getEncompassingRectColliderLabel()
-{
-return m_EncompassingRectColliderLabel;
+string GameObjectBlueprint::getEncompassingRectColliderLabel() {
+	return m_EncompassingRectColliderLabel;
 }
 
-bool GameObjectBlueprint::getEncompassingRectCollider()
-{
-return m_EncompassingRectCollider;
+bool GameObjectBlueprint::getEncompassingRectCollider() {
+	return m_EncompassingRectCollider;
 }
 
-void GameObjectBlueprint::setEncompassingRectCollider(string label)
-{
-m_EncompassingRectCollider = true;
-m_EncompassingRectColliderLabel = "" + label;
+void GameObjectBlueprint::setEncompassingRectCollider(string label) {
+	m_EncompassingRectCollider = true;
+	m_EncompassingRectColliderLabel = "" + label;
 }
