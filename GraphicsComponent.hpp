659bd030217b639b6c1ed9cefd5fd9da5c37babf
amaraoussama94 <wassa36@yahoo.fr>
2022-11-23 15:10:02 +0100

    fixing bugs

--- GraphicsComponent.hpp
+++ GraphicsComponent.hpp
@@ -1,47 +1,52 @@
 #pragma once
 #include "Component.hpp"
 #include "TransformComponent.hpp"
-#include "GameObjectSharer.hpp"
-
-#include <SFML/Graphics.hpp>
-
 #include <string>
+#include <SFML/Graphics.hpp>
+#include "GameObjectSharer.hpp"
 #include <iostream>
-#include <memory>
 
 using namespace sf;
 using namespace std;
 
-
 class GraphicsComponent : public Component {
 private:
-    string m_Type = "graphics";
-    bool m_Enabled = false;
-public:
-    virtual void draw(RenderWindow& window,shared_ptr<TransformComponent> t) = 0;//shared_ptr :vital data such as position and scale can be accessed each frame of the game
-    virtual void initializeGraphics(string bitmapName,Vector2f objectSize) = 0;
+	string m_Type = "graphics";
+	bool m_Enabled = false;
 
-    /****************************************************
-    *****************************************************
-    From Component interface
-    *****************************************************
-    *****************************************************/
-    string  getType() 
-    {
-        return m_Type;
-    }
-    void  disableComponent() 
-    {
-        m_Enabled = false;
-    }
-    void  enableComponent() 
-    {
-        m_Enabled = true;
-    }
-    bool  enabled() 
-    {
-        return m_Enabled;
-    }
-    void  start(GameObjectSharer* gos, GameObject* self) 
-    {}
+public:
+//shared_ptr :vital data such as position and scale can be accessed each frame of the game
+	virtual void draw(
+		RenderWindow& window,
+		shared_ptr<TransformComponent> t) = 0;
+
+	virtual void initializeGraphics(
+		string bitmapName,
+		Vector2f objectSize) = 0;
+
+	/****************************************************
+	*****************************************************
+	From Component interface
+	*****************************************************
+	*****************************************************/
+
+	string getType() {
+		return m_Type;
+	}
+
+	void disableComponent() {
+		m_Enabled = false;
+	}
+
+	void enableComponent() {
+		m_Enabled = true;
+	}
+
+	bool enabled() {
+		return m_Enabled;
+	}
+
+	void start(
+		GameObjectSharer* gos, GameObject* self) {}
+
 };
--- InvaderUpdateComponent.hpp
+++ InvaderUpdateComponent.hpp
@@ -1,60 +1,81 @@
- #pragma once
+#pragma once
 #include "UpdateComponent.hpp"
 #include "TransformComponent.hpp"
 #include "GameObjectSharer.hpp"
 #include "RectColliderComponent.hpp"
 #include "GameObject.hpp"
-#include<memory>
+
 class BulletSpawner;
+
 class InvaderUpdateComponent : public UpdateComponent
 {
 private:
-    string m_SpecificType = "invader";
-    shared_ptr<TransformComponent> m_TC; //  so that the invader can move
-    shared_ptr < RectColliderComponent> m_RCC;//update its location and be collided with
-
-    //query the position of the player and 
+	string m_SpecificType = "invader";
+//  so that the invader can move
+	shared_ptr<TransformComponent> m_TC;
+	//update its location and be collided with
+	shared_ptr < RectColliderComponent> m_RCC;
+	//query the position of the player and 
     //make decisions about when to shoot bullets
-    shared_ptr < TransformComponent> m_PlayerTC;
-    shared_ptr < RectColliderComponent> m_PlayerRCC;
+	shared_ptr < TransformComponent> m_PlayerTC;
+	shared_ptr < RectColliderComponent> m_PlayerRCC;
 
-    
-    BulletSpawner* m_BulletSpawner;
-    float m_Speed = 10.0f;
-    bool m_MovingRight = true;
-    float m_TimeSinceLastShot;
-    float m_TimeBetweenShots = 5.0f;
-    float m_AccuracyModifier;
-    float m_SpeedModifier = 0.05;
-    int m_RandSeed;
+	BulletSpawner* m_BulletSpawner;
+
+	float m_Speed = 10.0f;
+	bool m_MovingRight = true;
+	float m_TimeSinceLastShot;
+	float m_TimeBetweenShots = 5.0f;
+	float m_AccuracyModifier;
+	float m_SpeedModifier = 0.05;
+	int m_RandSeed;
+
 public:
-    void dropDownAndReverse();
-    bool isMovingRight();
-    void initializeBulletSpawner(BulletSpawner*
-    bulletSpawner, int randSeed);
-    
-    /****************************************************
-    *****************************************************
-    From Component interface base class
-    *****************************************************
-    *****************************************************/
-    string getSpecificType() 
-    {
-        return m_SpecificType;
-    }
-    void start(GameObjectSharer* gos,GameObject* self) 
-    {
-        // Where is the player?
-        m_PlayerTC = static_pointer_cast<TransformComponent>(gos->findFirstObjectWithTag("Player").getComponentByTypeAndSpecificType("transform", "transform"));
-        m_PlayerRCC = static_pointer_cast<RectColliderComponent>(gos->findFirstObjectWithTag("Player").getComponentByTypeAndSpecificType("collider", "rect"));
-        // Where is this specific invader
-        m_TC = static_pointer_cast<TransformComponent>(self->getComponentByTypeAndSpecificType("transform", "transform"));
-        m_RCC = static_pointer_cast<RectColliderComponent>(self->getComponentByTypeAndSpecificType("collider", "rect"));
-    }
-    /****************************************************
-    *****************************************************
-    From UpdateComponent
-    *****************************************************
-    *****************************************************/
-    void update(float fps) override;
+	void dropDownAndReverse();
+	bool isMovingRight();
+	void initializeBulletSpawner(BulletSpawner*
+		bulletSpawner, int randSeed);
+
+	/****************************************************
+	*****************************************************
+	From Component interface base class
+	*****************************************************
+	*****************************************************/
+
+	string  getSpecificType() {
+		return m_SpecificType;
+	}
+
+	void  start(GameObjectSharer* gos,
+		GameObject* self) {
+
+		// Where is the player?
+		m_PlayerTC = static_pointer_cast<TransformComponent>(
+			gos->findFirstObjectWithTag("Player")
+			.getComponentByTypeAndSpecificType(
+				"transform", "transform"));
+
+		m_PlayerRCC = static_pointer_cast<RectColliderComponent>(
+			gos->findFirstObjectWithTag("Player")
+			.getComponentByTypeAndSpecificType(
+				"collider", "rect"));
+
+		// Where is this specific invader
+		m_TC = static_pointer_cast<TransformComponent>(
+			self->getComponentByTypeAndSpecificType(
+				"transform", "transform"));
+
+		m_RCC = static_pointer_cast<RectColliderComponent>(
+			self->getComponentByTypeAndSpecificType(
+				"collider", "rect"));
+	}
+
+	/****************************************************
+	*****************************************************
+	From UpdateComponent
+	*****************************************************
+	*****************************************************/
+
+	void update(float fps) override;
 };
+
