c8b72e2586dc3c997aac2fe9ac7a390fc2a4df5a
amaraoussama94 <wassa36@yahoo.fr>
2022-11-23 14:57:56 +0100

    fixing bugs

--- ColliderComponent.hpp
+++ ColliderComponent.hpp
@@ -1,34 +1,39 @@
- #pragma once
+#pragma once
 #include "Component.hpp"
 #include <iostream>
+
 class ColliderComponent : public Component
 {
 private:
-string m_Type = "collider";
-bool m_Enabled = false;
+	string m_Type = "collider";
+	bool m_Enabled = false;
+
 public:
-/****************************************************
-*****************************************************
-From Component interface
-*****************************************************
-*****************************************************/
-virtual  string getType() 
-{
-return m_Type;
-}
-virtual void disableComponent() 
-{
-m_Enabled = false;
-}
-virtual void enableComponent()
- {
-m_Enabled = true;
-}
-virtual bool enabled() 
-{
-return m_Enabled;
-}
-virtual void  start(GameObjectSharer* gos, GameObject* self)
-{
-}
+
+	/****************************************************
+	*****************************************************
+	From Component interface
+	*****************************************************
+	*****************************************************/
+
+	string getType() {
+		return m_Type;
+	}
+
+	void disableComponent() {
+		m_Enabled = false;
+	}
+
+	void enableComponent() {
+		m_Enabled = true;
+	}
+
+	bool enabled() {
+		return m_Enabled;
+	}
+
+	void start(GameObjectSharer* gos, GameObject* self) {
+
+	}
 };
+
--- Component.hpp
+++ Component.hpp
@@ -1,18 +1,19 @@
- #pragma once
-#include "GameObjectSharer.hpp"
-#include <string>
-
 
 /******************This is the base class of every component in every game object.***********************/
+#pragma once
+#include "GameObjectSharer.hpp"
+#include <string>
+#include<memory> //used  for shared pointer
 using namespace std;
+
 class GameObject;
-class Component
-{
+
+class Component {
 public:
-    virtual string getType() = 0;
-    virtual string getSpecificType() = 0;
-    virtual void disableComponent() = 0;
-    virtual void enableComponent() = 0;
-    virtual bool enabled() = 0;
-    virtual void start(GameObjectSharer* gos, GameObject* self) = 0;
-    };
+	virtual string getType() = 0;
+	virtual string getSpecificType() = 0;
+	virtual void disableComponent() = 0;
+	virtual void enableComponent() = 0;
+	virtual bool enabled() = 0;
+	virtual void start(GameObjectSharer* gos, GameObject* self) = 0;
+};
